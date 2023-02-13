b765796df1f323027d6ab9ebfe0250f915981e10
xinetzone <xinzone@outlook.com>
2023-02-13 09:24:17 +0800

    设置 sphinx-intl 的一致性

--- doc/conf.py
+++ doc/conf.py
@@ -157,7 +157,7 @@ html_css_files = ["default.css"]
 
 # -- 国际化输出 ----------------------------------------------------------------
 
-locale_dirs = ['locales/']  # path is example but recommended.
+locale_dirs = ['../locales/']  # path is example but recommended.
 gettext_compact = False  # optional.
 
 # -- 主题设置 -------------------------------------------------------------------
